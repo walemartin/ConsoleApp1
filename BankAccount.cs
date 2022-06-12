de42d0e7dce5cb5b240560d6d1d0aefaa78bb16e
OLAWALE MARTINS <rytesoft@hotmail.com>
2022-06-12 17:03:56 +0100

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class BankAccount
+    {
+        private double balance;
+        public BankAccount()
+        {
+
+        }
+        public BankAccount(double balance)
+        {
+            this.balance = balance;
+        }
+        public double Balance
+        {
+            get { return balance; }
+        }
+        public void Add(double amount)
+        {
+            if(amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            balance+=amount;
+        }
+        public void WithDraw(double amount)
+        {
+            if(amount > balance) 
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            if(amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            balance -= amount;
+        }
+        public void TransferFundsTo(BankAccount otherAccount,double amount)
+        {
+            if(otherAccount is null)
+                throw new ArgumentNullException(nameof(otherAccount));
+            WithDraw(amount);
+            otherAccount.Add(amount);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Olawale!");
+            List<string> str = new List<string>();
+            string[] astr = { "Ola", "ola", "Tango" };
+            str.AddRange(astr);
+            Console.WriteLine(str[0].ToLower().Equals(str[1].ToLower()));
+            Console.ReadKey();
+        }
+    }
+}
